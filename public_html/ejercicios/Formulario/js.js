14e6c384233b6ff298c8f613105b285eb5dca9e0
Sergio <>
2020-03-24 13:08:33 +0100

    Revisado ejercicio Instagram, añadido ejercicio Formulario y pequeños cambios en el diseño del apartado de Ejercicios

---
+++ public_html/ejercicios/Formulario/js.js
@@ -0,0 +1,7 @@
+tinymce.init({
+    selector: '#editor',  // change this value according to your HTML
+    plugin: 'a_tinymce_plugin',
+    a_plugin_option: true,
+    a_configuration_option: 400
+  });
+  
